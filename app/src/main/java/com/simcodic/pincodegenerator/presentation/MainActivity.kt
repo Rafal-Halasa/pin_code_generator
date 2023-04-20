72abdc7ea9e0f42767a36886880eda35c4786c20
rhalasa <rafal.halasa@gmail.com>
2023-04-20 07:46:42 +0200

    add hilt injection

--- app/src/main/java/com/simcodic/pincodegenerator/presentation/MainActivity.kt
+++ app/src/main/java/com/simcodic/pincodegenerator/presentation/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.simcodic.pincodegenerator.presentation.pins_list.PinsListScreen
 import com.simcodic.pincodegenerator.presentation.theme.PinCodeGeneratorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
