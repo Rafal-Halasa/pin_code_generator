95942db2542d263316150811f81cb39a1ba1894c
rhalasa <rafal.halasa@gmail.com>
2023-04-23 18:09:23 +0200

    add pin list ordering

--- app/src/main/java/com/simcodic/pincodegenerator/data/data/PinCode.kt
+++ app/src/main/java/com/simcodic/pincodegenerator/data/data/PinCode.kt
@@ -5,14 +5,16 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import  com.simcodic.pincodegenerator.domain.data.PinCode as PinCodeDomain
 
-const val PIN_CODE_TABLE_NAME = "pin_code"
+const val PIN_CODE_TABLE_NAME = "pin_code_table"
+const val PIN_CODE_COLUMN_PIN_NAME = "pin_name"
+const val PIN_CODE_COLUMN_PIN_CODE = "pin_code"
 
 @Entity(tableName = PIN_CODE_TABLE_NAME)
 data class PinCode(
 
     @PrimaryKey(autoGenerate = true) val uid: Int = 0,
-    @ColumnInfo(name = "pin_name") val name: String,
-    @ColumnInfo(name = "pin_code") val pinCode: String
+    @ColumnInfo(name = PIN_CODE_COLUMN_PIN_NAME) val name: String,
+    @ColumnInfo(name = PIN_CODE_COLUMN_PIN_CODE) val pinCode: String
 )
 
 fun PinCode.toDomainModel() = PinCodeDomain(id = uid, name = name, pinCode = pinCode)
--- app/src/main/java/com/simcodic/pincodegenerator/data/repositories/local/dao/PinCodeDao.kt
+++ app/src/main/java/com/simcodic/pincodegenerator/data/repositories/local/dao/PinCodeDao.kt
@@ -4,13 +4,14 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import com.simcodic.pincodegenerator.data.data.PIN_CODE_COLUMN_PIN_NAME
 import com.simcodic.pincodegenerator.data.data.PIN_CODE_TABLE_NAME
 import com.simcodic.pincodegenerator.data.data.PinCode
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PinCodeDao {
-    @Query("SELECT * FROM $PIN_CODE_TABLE_NAME")
+    @Query("SELECT * FROM $PIN_CODE_TABLE_NAME ORDER BY $PIN_CODE_COLUMN_PIN_NAME ASC;")
     fun getAll(): Flow<List<PinCode>>
 
     @Insert
